e0e51eae8f5ad182d8f49bad3f50e7388bbf74e6
gauravyadv <gauravyadavry_cse18@its.edu.in>
2024-02-28 14:19:35 +0530

    .

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors(
- { origin: "http://localhost:3002",
+ { origin: ["http://localhost:3002","https://my-portfolio-4ghn.vercel.app"],
  credentials:true }
 ));
 
